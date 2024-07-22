2744289525943b37896748e34133a0bc8ac69248
emmanuelle giffard <emmanuelle.giffard@gmail.com>
2024-07-22 18:05:32 +0200

    form validations implemented

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,11 +11,10 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const closeBtn = document.querySelector('.close')
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
 
 // launch modal form
 function launchModal() {
@@ -26,6 +25,34 @@ function launchModal() {
 closeBtn.addEventListener("click", closeModal);
 
 // close modal form
-function  closeModal() {
+function closeModal() {
   modalbg.style.display = "none";
+}
+
+// Form validation
+function validateForm() {
+  const form = document.forms["reserve"];
+  const radios = form["location"];
+  let radioSelected = false;
+  const checkbox1 = document.getElementById("checkbox1");
+  if (!form.checkValidity()) {
+    return false;
+  }
+
+  radios.forEach((radio) => {
+    if (radio.checked) {
+      radioSelected = true;
+    }
+  });
+
+  if (!radioSelected) {
+    alert("Veuillez sélectionner un tournoi.");
+    return false;
+  }
+
+  if (!checkbox1.checked) {
+    alert("Veuillez sélectionner un tournoi.");
+    return false;
+  }
+  return true;
 }
