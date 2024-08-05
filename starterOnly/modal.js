ae39ef512386761da723c9792000ed1e0d6b3e2c
emmanuelle giffard <emmanuelle.giffard@gmail.com>
2024-08-06 01:07:12 +0200

    catch error in js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,153 +1,204 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
-// DOM Elements
+// Éléments du DOM
 const modalbg = document.querySelector(".bground");
+const modalForm = document.getElementById("modal-form");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
+const modalSuccess = document.getElementById("modal-success");
+const closeSuccessBtns = modalSuccess.querySelectorAll(".closemodalsuccess");
 
-// launch modal event
+// Messages d'erreur
+const errorMessages = {
+  "error-first":
+    "Veuillez entrer 2 caractères ou plus pour le champ du prénom.",
+  "error-last": "Veuillez entrer 2 caractères ou plus pour le champ du nom.",
+  "error-email": "Veuillez entrer une adresse mail valide.",
+  "error-birthdate": "Veuillez entrer une date de naissance valide.",
+  "error-quantity":
+    "Veuillez répondre à cette question (entre 0 et 99 tournois).",
+  "error-location": "Veuillez choisir au moins une ville.",
+  "error-checkbox1":
+    "Vous devez vérifier que vous acceptez les termes et conditions.",
+};
+
+// Événement pour ouvrir la modal
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// Fonction pour ouvrir la modal
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// close modal event
+// Événement pour fermer la modal
 closeBtn.addEventListener("click", closeModal);
 
-// close modal form
+// Fonction pour fermer la modal
 function closeModal() {
   modalbg.style.display = "none";
+  resetForm(); // Réinitialiser les champs du formulaire en fermant la modal
   isValid = true;
   document
     .querySelectorAll(".error-message")
     .forEach((el) => (el.style.display = "none"));
 }
 
-// Form validation
+// Fonction pour réinitialiser les champs du formulaire
+function resetForm() {
+  modalForm.reset();
+}
+
+// Validation du formulaire
 let isValid = true;
 
+// Fonction pour obtenir un élément par son ID
 function getElement(id) {
   return document.getElementById(id);
 }
 
+// Fonction pour afficher les messages d'erreur
 function showError(errorId) {
-  return (document.getElementById(errorId).style.display = "block");
+  const errorElement = document.getElementById(errorId);
+  errorElement.textContent = errorMessages[errorId];
+  errorElement.style.display = "block";
 }
 
 const form = document.forms["reserve"];
 
+// Fonction de validation
 function validate() {
   isValid = true;
-  document
-    .querySelectorAll(".error-message")
-    .forEach((el) => (el.style.display = "none"));
+  document.querySelectorAll(".error-message").forEach((el) => {
+    el.textContent = "";
+    el.style.display = "none";
+  });
+
+  // Tableau pour collecter les erreurs
+  const errors = [];
 
   // Validation du prénom
-  const first = document.getElementById("first");
-  if (!first.validity.valid) {
-    showError("error-first");
-    isValid = false;
+  try {
+    const first = getElement("first");
+    if (!first.validity.valid) {
+      throw new Error("error-first");
+    }
+  } catch (error) {
+    errors.push(error.message);
   }
 
   // Validation du nom
-  const last = getElement("last");
-  if (!last.validity.valid) {
-    showError("error-last");
-    isValid = false;
+  try {
+    const last = getElement("last");
+    if (!last.validity.valid) {
+      throw new Error("error-last");
+    }
+  } catch (error) {
+    errors.push(error.message);
   }
 
-  // Validation du mail
-  const email = getElement("email");
-  if (email.validity.valueMissing || email.validity.typeMismatch) {
-    showError("error-email");
-    isValid = false;
+  // Validation de l'email
+  try {
+    const email = getElement("email");
+    if (email.validity.valueMissing || email.validity.typeMismatch) {
+      throw new Error("error-email");
+    }
+  } catch (error) {
+    errors.push(error.message);
   }
 
   // Validation de la date de naissance
-  const birthDate = getElement("birthdate");
-  const today = new Date();
-  const birthDateValue = new Date(birthDate.value);
-  const age = today.getFullYear() - birthDateValue.getFullYear();
-  if (!age || age < 15 || age > 99 || birthDateValue > today) {
-    showError("error-birthdate");
-    isValid = false;
+  try {
+    const birthDate = getElement("birthdate");
+    const today = new Date();
+    const birthDateValue = new Date(birthDate.value);
+    const age = today.getFullYear() - birthDateValue.getFullYear();
+    if (!age || age < 15 || age > 99 || birthDateValue > today) {
+      throw new Error("error-birthdate");
+    }
+  } catch (error) {
+    errors.push(error.message);
   }
 
   // Validation de la quantité
-  const quantity = getElement("quantity")
-  if (!quantity.value || quantity.value < 0 || quantity.value > 99) {
-    console.log("quantity")
-    showError("error-quantity");
-    isValid = false;
-  }
-
-  // Validation des radios
-  const radios = form["location"];
-  let radioSelected = false;
-
-  radios.forEach((radio) => {
-    if (radio.checked) {
-      radioSelected = true;
+  try {
+    const quantity = getElement("quantity");
+    if (!quantity.value || quantity.value < 0 || quantity.value > 99) {
+      throw new Error("error-quantity");
     }
-  });
-
-  if (!radioSelected) {
-    document.getElementById("error-location").style.display = "block";
-    isValid = false;
+  } catch (error) {
+    errors.push(error.message);
   }
 
-  // Validation des conditions
-  const checkbox1 = getElement("checkbox1");
-  if (!checkbox1.checked) {
-    document.getElementById("error-checkbox1").style.display = "block";
-    isValid = false;
-  }
+  // Validation des boutons radio
+  try {
+    const radios = form["location"];
+    let radioSelected = false;
 
-  if (isValid) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-form.addEventListener("submit", (event) => {
-  if (!validate()) {
-    event.preventDefault();
-  } else {
-    event.preventDefault();
-    const modalSuccess = getElement("modalsuccess")
-
-     // Get heights
-     const modalBgHeight = modalbg.offsetHeight;
-     const formHeight = form.offsetHeight;
-
-     // Apply heights
-     modalSuccess.style.height = modalBgHeight + "px";
-     modalSuccess.querySelector(".content").style.height = formHeight + "px";
-
-    modalSuccess.style.display="block"
-    closeModal();
-
-    console.log("success");
-    const closeSuccessModal = getElement('closemodalsuccess');
-    const closeSuccessBtn = getElement('closemodal');
-
-    closeSuccessModal.addEventListener('click', () => {
-      modalSuccess.style.display = 'none';
+    radios.forEach((radio) => {
+      if (radio.checked) {
+        radioSelected = true;
+      }
     });
 
-    closeSuccessBtn.addEventListener('click', () => {
-      modalSuccess.style.display = 'none';
+    if (!radioSelected) {
+      throw new Error("error-location");
+    }
+  } catch (error) {
+    errors.push(error.message);
+  }
+
+  // Validation de la case à cocher
+  try {
+    const checkbox1 = getElement("checkbox1");
+    if (!checkbox1.checked) {
+      throw new Error("error-checkbox1");
+    }
+  } catch (error) {
+    errors.push(error.message);
+  }
+
+  // Afficher les erreurs
+  if (errors.length > 0) {
+    errors.forEach((error) => showError(error));
+    isValid = false;
+  }
+
+  return isValid;
+}
+
+// Événement lors de la soumission du formulaire
+form.addEventListener("submit", (event) => {
+  if (!validate()) {
+    // Annuler la soumission du formulaire en cas d'erreur
+    event.preventDefault();
+  } else {
+    // Annuler la soumission par défaut
+    event.preventDefault();
+
+    // Obtenir la hauteur du formulaire
+    const formHeight = modalForm.offsetHeight;
+
+    // Cahcher le formulaire
+    modalForm.style.display = "none";
+
+    // Afficher la modale de succès
+    modalSuccess.style.display = "flex";
+
+    // Appliquer la hauteur à la modal de succès
+    modalSuccess.style.height = formHeight + "px";
+
+    // Fonction pour soumettre le formulaire après la fermeture de la modal
+    function submitForm() {
+      form.submit();
+      console.log("Formulaire soumis avec succès !");
+    }
+    submitForm();
+
+    // Ajouter des événements pour fermer la modal de succès
+    closeSuccessBtns.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        modalSuccess.style.display = "none";
+        closeModal();
+      });
     });
   }
 });
